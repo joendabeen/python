939de15c8de19dcbfcdf7ce9234fcc281d4689bc
jeondabeen <jeondabeen@gmail.com>
2026-04-16 11:29:49 +0900

    Add 02.py file

---
+++ 02_print_func.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+
+age = 20
+name = "홍길동"
+print(name, age)
+print("이름은", name, "나이는", age)
+print("이름은" + name + "나이는" + str(age))
--- data_type.py
+++
@@ -1,4 +0,0 @@
-print(type(10))
-print(type(1.1))
-s = "Hello"
-print(type(s))
