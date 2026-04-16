60ec937523d012799122f090122b263961332bbd
jeondabeen <jeondabeen@gmail.com>
2026-04-16 11:14:02 +0900

    Start python study

---
+++ op.py
@@ -0,0 +1,17 @@
+print(1/2)
+print(5//3)
+print(5**2)
+print(5**3)
+print(20%3)
+
+print('Hello'*2)
+
+num=2
+num**=2
+print(num)
+
+hello = 'Hello'
+hello += 'World!'
+print(hello)
+hello *= 3
+print(hello)
